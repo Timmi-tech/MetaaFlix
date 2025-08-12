76d5e9a5e7f50587f8645a167409d949bc61f5ce
Timmi-tech <tomiwatomzy66@gmail.com>
2025-08-12 20:05:38 +0100

    Fixed the issue

--- CloudClip.Extensions/ServiceExtensions.cs
+++ CloudClip.Extensions/ServiceExtensions.cs
@@ -50,7 +50,7 @@ namespace StreamNest.Extensions
             {
                 options.AddPolicy("CorsPolicy", builder =>
                 {
-                    builder.WithOrigins("https://clipcloud-gamma.vercel.app","http://localhost:3001", "http://localhost:3000" )
+                    builder.WithOrigins("http://localhost:3001", "http://localhost:3000", "https://metaflix-eosin.vercel.app" )
                            .AllowAnyMethod()
                            .AllowAnyHeader()
                           .AllowCredentials();
